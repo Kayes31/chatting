0b5a6a042dfac35cc33c92f91f8587b6ed57357b
Kayes31 <136086147+Kayes31@users.noreply.github.com>
2026-04-16 14:25:41 +0600

    Update route.ts

--- my-app/app/api/chat/route.ts
+++ my-app/app/api/chat/route.ts
@@ -5,12 +5,9 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-
-  const result = await streamText({
-    // Use the 'google' provider function imported from @ai-sdk/google
-    model: google("models/gemini-1.5-flash"), 
+  const result = streamText({
+    model: google("gemini-2.0-flash"),
     messages: convertToModelMessages(messages),
   });
-
-  return result.toDataStreamResponse();
+  return result.toUIMessageStreamResponse();
 }
